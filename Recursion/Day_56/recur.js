d3788c049a70d979e842808d9fa01e41fe80e7a3
priyansh sahu <sahupriyansh298@gmail.com>
2025-04-19 23:03:13 +0530

    fifth

---
+++ Recursion/Day_56/recur.js
@@ -0,0 +1,18 @@
+// factorial of n number
+
+function nfactorial(n) {     // 0(n) // 0(n)
+    if (n === 0) return 1;
+    return n * nfactorial(n - 1);
+}
+
+var n = 4;
+console.log(nfactorial(n)); // Output: 24
+
+// sum of n Number
+
+function sumofN(n) {        // 0(n) // 0(n)
+    if (n === 0) return 1;
+    return n + sumofN(n - 1);
+}
+var n = 5;
+console.log(sumofN(n));
