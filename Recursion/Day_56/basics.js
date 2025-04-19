b408ce37d71aaf1be3d78e5331abefb3c2c5ff0b
priyansh sahu <sahupriyansh298@gmail.com>
2025-04-19 17:02:44 +0530

    basic

--- Recursion/Day_56/basics.js
+++ Recursion/Day_56/basics.js
@@ -21,3 +21,24 @@ function temp(a) {
 temp(10)
 
 // recursion 
+
+var n = 5;
+
+temp1(n)         //    stack overflow // run
+function temp1(a) {   
+       if(a == 0) return              
+   console.log("Pbks");
+   temp1(a-1)              // run
+}
+// a++
+
+var n = 5;
+printNums(n)
+function printNums(a) {
+    if(a === 0) return 
+    process.stdout.write(a + " ")  // n to 1 --> n, n-1, n-2, n-3, n-4, n-5
+    printNums(a-1)
+}
+
+
+
