458262309c778c234f5581e60ebe09407a61c6b9
priyansh sahu <sahupriyansh298@gmail.com>
2025-05-01 16:23:12 +0530

    duplicat

--- Recursion/Day_63/remove_duplicate.js
+++ Recursion/Day_63/remove_duplicate.js
@@ -7,6 +7,23 @@ function removeDuplicates(nums) {
             j++;
         }
     }
-    return nums;
+    return nums;  
+};
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let j = 1; // Pointer for placing unique elements
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] !== nums[i + 1]) {
+            nums[j] = nums[i + 1]; // Place the next unique element
+            j++;
+        }
+    }
+    return j; // Return the number of unique elements
 };
 console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
