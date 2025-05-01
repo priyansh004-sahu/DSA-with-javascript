3e31d4666e73b7506c4c0173b8a647c2c1ff4b8f
priyansh sahu <sahupriyansh298@gmail.com>
2025-05-01 11:30:52 +0530

    move

--- Recursion/Day_63/move_zeros.js
+++ Recursion/Day_63/move_zeros.js
@@ -1,2 +1,21 @@
 
-// 283
+// 283. Move Zeroes
+
+function moveZeroes(nums) {
+    let idx = 0;
+    for(var i = 0; i<nums.length; i++) {
+        if(nums[i] != 0) {
+            nums[idx] = nums[i];
+            idx++;
+        }
+    }  
+    
+    // // Second pass: fill the rest with 0s
+
+    for(var i=idx; i<nums.length; i++) {
+       nums[i] = 0;
+    }
+    return nums
+};
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
