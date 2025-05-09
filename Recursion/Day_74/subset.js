8687f70925fed97145bb44ee4e8606ba1c8ba06d
priyansh sahu <sahupriyansh298@gmail.com>
2025-05-09 23:52:15 +0530

    subst

---
+++ Recursion/Day_74/subset.js
@@ -0,0 +1,30 @@
+
+
+//  1089. Duplicate Zeros
+
+function DuplicateZeros(arr) {
+    let zeros = 0;
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i] == 0) zeros++;
+    }
+
+    let i = arr.length-1;
+    let j = (arr.length-1) + zeros;
+
+    while(i != j) {
+        if(j < arr.length) {
+            arr[j] = arr[i]
+        }
+        j--;
+
+        if(arr[i] == 0) {
+            if(j < arr.length) {
+                arr[j] = arr[i];
+            }
+        j--;
+        }
+        i--;
+    }
+ return arr;
+}
+console.log(DuplicateZeros([1,0,2,3,0,4]));
