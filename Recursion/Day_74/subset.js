b51b09d69b07d8542502853ee8ada0d136f0dcd7
priyansh sahu <sahupriyansh298@gmail.com>
2025-05-10 17:39:30 +0530

    subset

--- Recursion/Day_74/subset.js
+++ Recursion/Day_74/subset.js
@@ -1,30 +1,30 @@
 
+function getAllSubsets(arr, ans, i, allSubsets) {
+  if(i == arr.length) {
+    // store subset
+    allSubsets.push([...ans])
+    return;
+  }
 
-//  1089. Duplicate Zeros
+  // include
+  ans.push(arr[i]);
+  getAllSubsets(arr, ans, i+1, allSubsets);
 
-function DuplicateZeros(arr) {
-    let zeros = 0;
-    for(let i=0; i<arr.length; i++) {
-        if(arr[i] == 0) zeros++;
-    }
+  ans.pop();
+  //   exclude
+ getAllSubsets(arr, ans, i+1, allSubsets);
 
-    let i = arr.length-1;
-    let j = (arr.length-1) + zeros;
 
-    while(i != j) {
-        if(j < arr.length) {
-            arr[j] = arr[i]
-        }
-        j--;
-
-        if(arr[i] == 0) {
-            if(j < arr.length) {
-                arr[j] = arr[i];
-            }
-        j--;
-        }
-        i--;
-    }
- return arr;
 }
-console.log(DuplicateZeros([1,0,2,3,0,4]));
+
+
+function subSets(arr, ans, i) { 
+
+ var arr = [1, 2, 3];
+ var ans = [];
+  var allSubsets = new Array();
+ getAllSubsets(arr, ans, 0, allSubsets)
+ console.log(allSubsets);
+ 
+}
+subSets()
