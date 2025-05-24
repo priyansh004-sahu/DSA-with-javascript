775651ae9281d6febe4fea6797718558d7a05683
priyansh sahu <sahupriyansh298@gmail.com>
2025-05-24 23:10:54 +0530

    missing

---
+++ Sorting_Algo/Day_85/missing_num.js
@@ -0,0 +1,19 @@
+function missingNumber(nums) {
+    let i = 0;
+    while(i < nums.length) {
+        let correctIdx = nums[i];
+        if(nums[i] < nums.length && nums[i] !== nums[correctIdx]) {
+            [nums[i], nums[correctIdx]] = [nums[correctIdx], nums[i]]; // swap
+        } else {
+            i++;
+        }  
+    }
+
+    for(let j = 0; j < nums.length; j++) {
+        if(j !== nums[j]) return j;
+    }
+
+    return nums.length;
+}
+
+console.log(missingNumber([3, 1, 0])); // Output: 2
