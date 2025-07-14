301f70623626b2bc90df275d1ab74cd2dba51d71
priyansh sahu <sahupriyansh298@gmail.com>
2025-07-14 21:46:15 +0530

    BT

---
+++ Binary_Tree/Day_4/tempCodeRunnerFile.js
@@ -0,0 +1,98 @@
+class Node {
+    constructor(val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+// Insert into BST
+function Insert(root, val) {
+    if (root == null) {
+        return new Node(val);
+    }
+
+    if (val < root.data) {
+        root.left = Insert(root.left, val);
+    } else {
+        root.right = Insert(root.right, val);
+    }
+
+    return root;
+}
+
+// Build BST from array
+function buildBST(arr) {
+    let root = null;
+
+    for (let val of arr) {
+        root = Insert(root, val);
+    }
+
+    return root;
+}
+
+// Inorder Traversal
+function inorder(root) {
+    if (root == null) return;
+
+    inorder(root.left);
+    console.log(root.data);
+    inorder(root.right);
+}
+
+function SearchInBST(root, key) {
+    if(root == null) return false;
+    if(root.data == key) return true;
+
+    if(root.data > key) {
+      return SearchInBST(root.left, key);
+    } else {
+        return SearchInBST(root.right, key);
+    }
+}
+
+function getInorderSuccessor(root) {      /// left most node in right subtree
+    while(root != null && root.left != null) {
+        root = root.left;
+    }
+    return root;
+}
+
+function delNode(root, key) { // key => val
+    if(root == null) {
+        return null;
+    }
+    if(key < root.data) {
+        root.left = delNode(root.left, key);
+    } else if(key > root.data){
+         root.right = delNode(root.right, key);
+    } else {
+        // key == root
+        if(root.left == null) {
+            let temp = root.right;
+            delete root;
+            return temp;
+        } else if(root.right == null) {
+            let temp = root.left;
+            delete root;
+            return temp;
+        } else { // 2 children
+            let IS = getInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delNode(root.right, IS.data);
+        }
+    }
+     return root;
+}
+// Example
+const arr = [3, 2, 1, 5, 6, 4];
+let root = buildBST(arr);
+inorder(root);
+let key = 6;
+
+delNode(6);
+inorder(root);
+
+
+console.log(SearchInBST(root, key))
